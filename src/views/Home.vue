ad6da858b173222cca4d0eaa3c6c19628682182f
sunchi <rachanlamoolphal@gmail.com>
2021-12-09 22:03:35 +0700

    menu and banner finish

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="w-full relative">
+       <Menu />
+       <Banner />
+   </div>
+</template>
+
+<script setup lang="ts">
+import Banner from '../components/home/Herobanner.vue'
+import Menu from '../components/layouts/Menu.vue'
+</script>
+
+<style>
+
+</style>

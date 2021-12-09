a1df9be41df32d841ee6f1255550bc09e05c0166
Un123 <thanakone2002@gmail.com>
2021-12-09 22:05:58 +0700

    sponsor-component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
-
 <script setup lang="ts">
- 
+import Sponsor from './components/sponsor.vue' 
 </script>
 
 <template>
- 
+    <Sponsor />
 </template>
 
 <style>
---
+++ src/components/sponsor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container font-mono m-auto">
+        <div style="background-color:6190be" class="text-center text-lg font-bold text-blue-500 mt-2">Brands</div>
+        <div class="text-4xl font-extrabold mt-6">
+            <div class="text-center">Get matched with the life insurance policy</div>
+            <div class="text-center">that works best for you</div>
+        </div>
+        <div class="text-center text-gray-500 center-text mt-6">with a few basic questions, you can search and compare personalized quotes from top carriers.</div>
+        <div>
+             <div id="sponsor-logo" class="mt-6 flex justify-center items-center">
+                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/facebook.png" alt="facebook-logo"></a></div>
+                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/google.png" alt="google-logo"></a></div>
+                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/instagram.png" alt="instagram-logo"></a></div>
+                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/twitter.png" alt="twitter-logo"></a></div>
+                <div class="h-12 w-12 ml-5 flex items-center"><a href=""><img src="../assets/image/sponsor-img/youtube.png" alt="youtube-logo"></a></div>
+            </div>
+        </div>
+        <div class="flex justify-center my-10">
+            <button style="background-color: #e94a82" class="px-6 py-2 font-medium text-sm hover:bg-red-600 text-red-100 rounded">Get Started</button>
+        </div>
+    </div>
+</template>
+
+<style>
+.text-center{
+    text-align: center;
+}
+
+.item-center{
+    right:50%;
+    transform: translateX(50%);
+}
+</style>

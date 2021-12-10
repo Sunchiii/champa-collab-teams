7604e60829db261ea50a199f948ded5b594afa25
Un123 <thanakone2002@gmail.com>
2021-12-10 21:36:05 +0700

    sponser-page-component

--- src/components/sponsor.vue
+++ src/components/sponsor.vue
@@ -1,22 +1,25 @@
 <template>
-    <div class="container font-mono m-auto">
-        <div style="background-color:6190be" class="text-center text-lg font-bold text-blue-500 mt-2">Brands</div>
-        <div class="text-4xl font-extrabold mt-6">
-            <div class="text-center">Get matched with the life insurance policy</div>
-            <div class="text-center">that works best for you</div>
-        </div>
-        <div class="text-center text-gray-500 center-text mt-6">with a few basic questions, you can search and compare personalized quotes from top carriers.</div>
-        <div>
-             <div id="sponsor-logo" class="mt-6 flex justify-center items-center">
-                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/facebook.png" alt="facebook-logo"></a></div>
-                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/google.png" alt="google-logo"></a></div>
-                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/instagram.png" alt="instagram-logo"></a></div>
-                <div class="h-12 w-12 ml-5"><a href=""><img src="../assets/image/sponsor-img/twitter.png" alt="twitter-logo"></a></div>
-                <div class="h-12 w-12 ml-5 flex items-center"><a href=""><img src="../assets/image/sponsor-img/youtube.png" alt="youtube-logo"></a></div>
+    <div class="container font-sans m-auto pb-20 px-2 w-full">
+        <div class="h-1/2">
+            <div style="color:6190be" class="pb-4 pt-1 text-center tracking-0.1em text-sm font-bold text-blue-500">BRANDS</div>
+            <div class="text-3xl font-bold pb-4">
+                <div class="text-center">Get matched with the life insurance policy</div>
+                <div class="text-center">that works best for you.</div>
             </div>
+            <div class="text-center text-gray-500 center-text text-base pb-10">with a few basic questions, you can search and compare personalized quotes from top carriers.</div>
         </div>
-        <div class="flex justify-center my-10">
-            <button style="background-color: #e94a82" class="px-6 py-2 font-medium text-sm hover:bg-red-600 text-red-100 rounded">Get Started</button>
+        
+        <div class="h-1/2">
+                <div id="sponsor-logo" class="flex justify-center items-center pb-10">
+                    <a href=""><div class="h-12 w-12 p-2"><img src="../assets/image/sponsor-img/facebook.png" alt="facebook-logo"></div></a>
+                    <a href=""><div class="h-12 w-12 p-2"><img src="../assets/image/sponsor-img/google.png" alt="google-logo"></div></a>
+                    <a href=""><div class="h-12 w-12 p-2"><img src="../assets/image/sponsor-img/instagram.png" alt="instagram-logo"></div></a>
+                    <a href=""><div class="h-12 w-12 p-2"><img src="../assets/image/sponsor-img/twitter.png" alt="twitter-logo"></div></a>
+                    <a href=""><div class="h-12 w-12 p-2 flex items-center"><img src="../assets/image/sponsor-img/youtube.png" alt="youtube-logo"></div></a>
+                </div>
+            <div class="flex justify-center">
+                <button style="background-color: #e94a82" class="px-6 py-2 font-medium text-sm text-red-100 rounded">Get Started</button>
+            </div>
         </div>
     </div>
 </template>
@@ -24,10 +27,5 @@
 <style>
 .text-center{
     text-align: center;
-}
-
-.item-center{
-    right:50%;
-    transform: translateX(50%);
 }
 </style>

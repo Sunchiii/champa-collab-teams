40ca3a40d1b3839cfab9fb6f1daa987280e998c9
Jay <jjaysocool1010@gmail.com>
2021-12-12 20:04:31 +0700

    #4 financial-services by jay is Done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 
 <script setup lang="ts">
- 
+// import from components-financial_services
+import Financial_services from './components/financial_service.vue'
 </script>
-
+    
 <template>
- 
+    <Financial_services/>
 </template>
 
 <style>

f21cec20f1069f1201e68c109278116c19c4afab
sunchi <rachanlamoolphal@gmail.com>
2021-12-14 22:27:13 +0700

    add footer banch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
        <Sponsor />
        <Safety />
 
+       <Footer />
    </div>
 </template>
 
@@ -20,6 +21,7 @@ import FiService from '../components/home/financial_service.vue'
 import Sponsor from '../components/home/sponsor.vue'
 import Safety from '../components/home/safety.vue'
 
+import Footer from '../components/layouts/footer.vue'
 </script>
 
 <style>
